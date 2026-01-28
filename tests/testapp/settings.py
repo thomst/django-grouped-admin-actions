c578de9283a34801ab41ad7efc240e953d5b7626
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-01-28 11:41:01 +0100

    we do not need to add the module to installed_apps

--- tests/testapp/settings.py
+++ tests/testapp/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'grouped_admin_actions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
